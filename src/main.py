658d7343ea25f99f7e02047e3c4328473943d702
Ahmed Al-Salali <a.s.alsalali@gmail.com>
2021-07-07 09:27:46 +0800

    added a small logic to add commits work hours and cleaned up the script

--- src/main.py
+++ src/main.py
@@ -2,73 +2,102 @@ from pygit2 import Repository, GIT_SORT_TOPOLOGICAL
 import datetime as dt
 import pytz
 
-class Author:
-  def __init__(self, name, email):
-    self.name = name
-    self.email = email
-    self.month = [self.Month()  for i in range(6)] 
 
-  #TODO: update this debug code
-  def __repr__(self) -> str:
-    return self.name + ' ' + str(getattr(self.month[3], 'weekDays'))
-
-
-  class Month:
+class workHours:
     def __init__(self) -> None:
-        self.weekDays = 0
-        self.weekends = 0
+        self.morning = 0
+        self.afterWork = 0
+        self.midNight = 0
 
-    def __setattr__(self, name, value):
-      self.__dict__[name] = value
-  
+    def increment(self, field):
+        self.__dict__[field] += 1
 
-  def addToWeekDays(self, targetMonth, num):
-    self.month[targetMonth].weekDays += num
 
-  def addToWeekends(self, targetMonth, num):
-    self.month[targetMonth].weekends += num
+class Month:
+    def __init__(self) -> None:
+        self.weekDays = workHours()
+        self.weekends = workHours()
 
+    def addCommit(self, name, hour):
+        self.__dict__[name].increment(hour)
+
+
+class Author:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.month = [Month() for i in range(6)]
+
+    # TODO: update this debug code
+    def __repr__(self) -> str:
+        return self.name + ' ' + str(self.month[3].weekDays.midNight)
+
+    def addToWeekDays(self, targetMonth, num):
+        self.month[targetMonth].weekDays += num
+
+    def addToWeekends(self, targetMonth, num):
+        self.month[targetMonth].weekends += num
 
 
 def addCollab(list, name, email):
 
-  for i in list:
-    if (i.name == name or i.email == email):
-      return i
+    for i in list:
+        if (i.name == name or i.email == email):
+            return i
 
-  list.append(Author(name, email))
-  return list[len(list) - 1] 
+    list.append(Author(name, email))
+    return list[len(list) - 1]
 
 
-def main(): 
-    #grab the repo name from the user
+def addToCommitHour(month, commitDate):
+    if commitDate.weekday() > 4:
+        day = 'weekDays'
+    else:
+        day = 'weekends'
+
+    if commitDate.hour in range(0, 8):
+        hour = 'midNight'
+    elif commitDate.hour in range(9, 6):
+        hour = 'workHours'
+    else:
+        hour = 'afterWork'
+
+    month.addCommit(day, hour)
+    return month
+
+
+def main():
+    # grab the repo name from the user
     repo = Repository('')
 
     collaraborators = []
 
-    #TODO: set the time zone from env
+    # TODO: set the time zone from env
 
     currentMonth = 0
     setFirst = False
     for commit in repo.walk(repo.head.target, GIT_SORT_TOPOLOGICAL):
-      if setFirst == False: 
-        inLoopMonth = dt.datetime.fromtimestamp(commit.commit_time, pytz.timezone('Asia/Kuala_Lumpur')).month
-        print(dt.datetime.fromtimestamp(commit.commit_time, pytz.timezone('Asia/Kuala_Lumpur')))
-        setFirst = True
+        commitDate = dt.datetime.fromtimestamp(
+            commit.commit_time, pytz.timezone('Asia/Kuala_Lumpur'))
+        if setFirst == False:
+            inLoopMonth = commitDate.month
+            setFirst = True
 
-      if inLoopMonth > dt.datetime.fromtimestamp(commit.commit_time, pytz.timezone('Asia/Kuala_Lumpur')).month: 
-        print(dt.datetime.fromtimestamp(commit.commit_time, pytz.timezone('Asia/Kuala_Lumpur')).month)
-        currentMonth += 1
-        inLoopMonth  = dt.datetime.fromtimestamp(commit.commit_time, pytz.timezone('Asia/Kuala_Lumpur')).month
+        if inLoopMonth > commitDate.month:
+            currentMonth += 1
+            inLoopMonth = commitDate.month
 
-      if(currentMonth > 5): break
+        if(currentMonth > 5):
+            break
 
-      coll = addCollab(collaraborators, commit.author.name, commit.author.email)
-      coll.addToWeekDays(currentMonth, 1)
+        coll = addCollab(collaraborators, commit.author.name,
+                         commit.author.email)
 
+        # analysis for the work time
+        addToCommitHour(coll.month[currentMonth], commitDate)
+        # coll.addToWeekDays(currentMonth, 1)
 
-    # for x in collaraborators:
-      # print (x.month[2].weekDays)
     print(collaraborators)
+
 
 main()
